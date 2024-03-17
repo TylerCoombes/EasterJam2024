e45af7b325c1bdfa8bf7d87abb3a85392faec517
Tyler Coombes <tylerjaycoombes@gmail.com>
2024-03-17 20:09:38 +0000

    Clamped Rotation
    Created function in CharacterController which clamps the rotation

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,6 +8,11 @@ public class CharacterController : MonoBehaviour
     private float y;
     public float sensitivity = -1f;
     private Vector3 rotate;
+    public float maxYRot = 30f;
+    public float minYRot = -30f;
+
+    public float maxXRot = 30f;
+    public float minXRot = -30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +26,20 @@ public class CharacterController : MonoBehaviour
         x = Input.GetAxis("Mouse Y");
         rotate = new Vector3 (x, y * sensitivity, 0);
         transform.eulerAngles = transform.eulerAngles - rotate;
+
+        LimitRot();
+    }
+
+    public void LimitRot()
+    {
+        Vector3 playerEulerAngles = transform.rotation.eulerAngles;
+
+        playerEulerAngles.y = (playerEulerAngles.y > 180) ? playerEulerAngles.y - 360 : playerEulerAngles.y;
+        playerEulerAngles.y = Mathf.Clamp(playerEulerAngles.y, minYRot, maxYRot);
+
+        playerEulerAngles.x = (playerEulerAngles.x > 180) ? playerEulerAngles.x - 360 : playerEulerAngles.x;
+        playerEulerAngles.x = Mathf.Clamp(playerEulerAngles.x, minXRot, maxXRot);
+
+        transform.localRotation = Quaternion.Euler(playerEulerAngles);
     }
 }
